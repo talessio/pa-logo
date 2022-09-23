3c3879a09b840208923106158a6f1460332254c9
Alessio Tozzi <81319268+talessio@users.noreply.github.com>
2022-09-23 15:34:25 +0200

    fixing folder

--- app/src/test/java/PA/logo/AppTest.java
+++
@@ -1,9 +0,0 @@
-package pa.logo;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class AppTest {
-
-}
--- app/src/test/java/PA/logo/ControllerTest.java
+++
@@ -1,5 +0,0 @@
-package pa.logo;
-
-public class ControllerTest {
-
-}
